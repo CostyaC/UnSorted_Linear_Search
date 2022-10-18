02571fced2bf70c048b8538c9ea262535c6240f2
CostyaC <114303055+CostyaC@users.noreply.github.com>
2022-10-18 23:23:37 +0200

    Add files via upload

---
+++ LinearSearch1.java
@@ -0,0 +1,19 @@
+package UnSorted_Linear_Search;
+
+public class LinearSearch1 {
+	
+	
+
+public int UnsortedLinearSearch(int A[], int n , int data){
+	
+for(int i = 0; i < n; i++){
+	
+if(A[i] == data)
+	
+	return i; }
+
+
+return -1; }
+	
+	
+}
---
+++ UnsortedLinear_Test.java
@@ -0,0 +1,18 @@
+package UnSorted_Linear_Search;
+
+public class UnsortedLinear_Test {
+
+
+public static void main(String[] args) {
+		
+int[] A ={4,8,20,9,5,12,14,3,6,25};	
+	
+LinearSearch1  UnsortedLinearTest = new LinearSearch1();
+
+
+System.out.println(UnsortedLinearTest.UnsortedLinearSearch(A , 10, 12));
+
+
+}
+
+}
